2420781031c7391aa1a6e23756681186c48847d9
Gabrielle Canlas <111622802+grbllcnls@users.noreply.github.com>
2024-01-13 15:13:45 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+img_height, img_width = 32, 32
+batch_size = 20
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "C://Users/Gabrielle/Downloads/fruits/fruits/train",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "C://Users/Gabrielle/Downloads/fruits/fruits/validation",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    "C://Users/Gabrielle/Downloads/fruits/fruits/test",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+class_names = ["Apple", "Banana", "Cabbage", "Cantaloupe", "Carrot", "Chayote", "Chili", "Egg", "Garlic", "Onion", "Orange", "Squash", "Tomato", "Watermelon"]
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    model = tf.keras.Sequential(
+    [
+     tf.keras.layers.Rescaling(1./255),
+     tf.keras.layers.Conv2D(32, 3, activation="relu"),
+     tf.keras.layers.MaxPooling2D(),
+     tf.keras.layers.Conv2D(32, 3, activation="relu"),
+     tf.keras.layers.MaxPooling2D(),
+     tf.keras.layers.Conv2D(32, 3, activation="relu"),
+     tf.keras.layers.MaxPooling2D(),
+     tf.keras.layers.Flatten(),
+     tf.keras.layers.Dense(128, activation="relu"),
+     tf.keras.layers.Dense(14) 
+    ]
+)
+    
+    model.compile(
+    optimizer="adam",
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits = True),
+    metrics=['accuracy']
+)
+    model.fit(
+    train_ds,
+    validation_data = val_ds,
+    epochs = 10
+)
+    model.evaluate(test_ds)
+    
+import numpy
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,10))
+for images, labels in test_ds.take(1):
+  classifications = model(images)
+  # print(classifications)
+  
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    index = numpy.argmax(classifications[i])
+    plt.title("Pred: " + class_names[index] + " | Real: " + class_names[labels[i]])
+    
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+with open("model.tflite", 'wb') as f:
+  f.write(tflite_model)
